f302b805fd1fba1fb70f9c4a3b35a4931ebec822
adi bangkit <adibangkai@gmail.com>
2023-02-20 03:06:15 +0700

    framer animate tl

--- components/Jejak.tsx
+++ components/Jejak.tsx
@@ -1,74 +1,64 @@
 "use client";
 import { useRef } from "react";
 import Timeline from "./Timeline";
+import { motion, useScroll, useSpring, animate } from "framer-motion";
+
 export default function Jejak() {
   const containerRef = useRef<HTMLDivElement>(null);
   return (
     <>
-      <div
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        // transition={{ delay: 1 }}
         ref={containerRef}
         className="px-0 md:px-40 snap-y snap-mandatory overflow-scroll top-0 h-screen  scroll-smooth"
       >
-        <div
-          id="jkt"
-          className="snap-always snap-start w-full md:w-5/6 px-10  pt-[30%] md:pt-[16%] h-screen transition-opacity duration-800 ease-in-out opacity-100 "
-        >
-          <div className="tanggal text-gray-300 ">21 April 2022 jkt</div>
-          <div className="quotes  text-2xl md:text-4xl font-extralight secondary-content ">
-            &quot; Banyak yang perlu ditingkatkan, khususnya kemasannya dan saya
-            liat juga banyak produk baru dari OK OCE,&quot;
-          </div>
-          <div className="sumber text-gray-300 font-extralight ">
-            (sumber: merdeka.com)
-          </div>
-        </div>
-        <div
-          id="sol"
-          className="snap-always snap-start w-full md:w-5/6 px-10 pt-[30%] md:pt-[16%] h-screen  transition-opacity duration-800 ease-in-out opacity-100 "
-        >
-          <div className="tanggal text-gray-300  ">21 April 2022 jkt</div>
-          <div className="quotes  text-2xl md:text-4xl font-extralight secondary-content ">
-            &quot; Banyak yang perlu ditingkatkan, khususnya kemasannya dan saya
-            liat juga banyak produk baru dari OK OCE,&quot;
-          </div>
-          <div className="sumber text-gray-300 font-extralight ">
-            (sumber: merdeka.com)
-          </div>
-        </div>
-        <div
-          id="bdg"
-          className="snap-always snap-start w-full md:w-5/6 px-10 h-screen pt-[30%] md:pt-[16%] transition-opacity duration-800 ease-in-out opacity-100 "
-        >
-          <div className="tanggal text-gray-300 ">21 April 2022 ok</div>
-          <div className="quotes  text-2xl md:text-4xl font-extralight secondary-content ">
-            &quot; Kenapa? Air itu turun dari langit ke bumi bukan ke laut.
-            Harusnya dimasukkan ke dalam bumi, masukkan ke tanah. Di seluruh
-            dunia, air jatuh dimasukkan ke tanah. Bukan dialirkan pakai
-            gorong-gorong raksasa ke laut. Jakarta telah mengambil keputusan
-            yang fatal...&quot;
-          </div>
-          <div className="sumber text-gray-300 font-extralight ">
-            (sumber: merdeka.com)
-          </div>
-        </div>{" "}
-        <div
-          id="oke"
-          className="snap-always snap-start w-full md:w-5/6 px-10 h-screen pt-[30%] md:pt-[16%] transition-opacity duration-800 ease-in-out opacity-100 "
-        >
-          <div className="tanggal text-gray-300 ">21 April 2022 jkt</div>
-          <div className="quotes  text-2xl md:text-4xl font-extralight secondary-content ">
-            &quot; Kenapa? Air itu turun dari langit ke bumi bukan ke laut.
-            Harusnya dimasukkan ke dalam bumi, masukkan ke tanah. Di seluruh
-            dunia, air jatuh dimasukkan ke tanah. Bukan dialirkan pakai
-            gorong-gorong raksasa ke laut. Jakarta telah mengambil keputusan
-            yang fatal...&quot;
-          </div>
-          <div className="sumber text-gray-300 font-extralight ">
-            (sumber: merdeka.com)
-          </div>
-        </div>
-      </div>
+        <Quotes containerRef={containerRef} />
+        <Quotes containerRef={containerRef} />
+        <Quotes containerRef={containerRef} />
+        <Quotes containerRef={containerRef} />
+      </motion.div>
       <Timeline containerRef={containerRef} />
     </>
+  );
+}
+type QuotesType = {
+  containerRef: React.RefObject<HTMLDivElement>;
+};
+function Quotes({ containerRef }: QuotesType) {
+  const scrollRef = useRef(null);
+  return (
+    <motion.div
+      viewport={{ root: containerRef }}
+      id="jkt"
+      className="snap-always snap-start w-full md:w-5/6 px-10  pt-[30%] md:pt-[16%] h-screen transition-opacity duration-800 ease-in-out opacity-100 "
+    >
+      <motion.div
+        initial={{ y: -150, opacity: 0.4 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.6 }}
+        className="tanggal text-base-300 font-light "
+      >
+        21 April 2022 jkt
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ delay: 0.3 }}
+        className="quotes  text-2xl md:text-4xl font-extralight secondary-content "
+      >
+        &quot; Banyak yang perlu ditingkatkan, khususnya kemasannya dan saya
+        liat juga banyak produk baru dari OK OCE,&quot;
+      </motion.div>
+      <motion.div
+        initial={{ y: -300, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1, type: "tween" }}
+        transition={{ delay: 0.8 }}
+        className="sumber text-gray-300 font-extralight "
+      >
+        (sumber: merdeka.com)
+      </motion.div>
+    </motion.div>
   );
 }
